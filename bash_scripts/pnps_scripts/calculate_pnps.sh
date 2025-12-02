b983849d3c2d2176c1eaacf65f00022903ca5f32
LucyDillon <62658632+LucyDillon@users.noreply.github.com>
2025-12-02 13:43:48 +0000

    Create calculate_pnps.sh

---
+++ bash_scripts/pnps_scripts/calculate_pnps.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#SBATCH --time=23:59:59
+#SBATCH --partition=k2-medpri
+#SBATCH --mem=100G
+#SBATCH --cpus-per-task=8
+#SBATCH --mail-user=l.dillon@qub.ac.uk
+#SBATCH --mail-type=END,BEGIN,FAIL
+#SBATCH --error=mag-utils%A-%a.err
+#SBATCH --job-name=mag-utils
+
+mags-utils calc -n Louise_ballymena  data output_pnps_calc.csv
